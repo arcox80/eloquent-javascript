e17f28d5bfad98a8a455d5ed894f8f87fd4b779e
Andrew Cox <drew.r.cox@gmail.com>
2022-03-13 22:02:18 -0400

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -38,7 +38,7 @@ for (let num = 1; num <= 100; num++) {
   }
 }
 
-//EJ Solution (doesn't include 'FizzBuzz' part):
+//EJ Solution:
 for (let n = 1; n <= 100; n++) {
   let output = "";
   if (n % 3 == 0) output += "Fizz";
@@ -47,5 +47,55 @@ for (let n = 1; n <= 100; n++) {
 }
 
 /*
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+Passing this string to console.log should show something like this:
 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 */
+
+//My solution:
+let size = 8;
+const chessBoard = function (size) {
+  for (let i = 1; i <= size; i++) {
+    let output = '';
+    let firstLine = ' #';
+    let secondLine = '# ';
+    for (let j = 2; j <= size; j += 2) {
+      if (i % 2 === 0) {
+        output += secondLine;
+      } else {
+        output += firstLine;
+      }
+    }
+    output += '\n';
+    console.log(output);
+  }
+};
+
+chessBoard(size);
+
+//EJ solution:
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
