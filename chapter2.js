8d4a02709228bca1e1b3e11de541227d847142f9
Andrew Cox <drew.r.cox@gmail.com>
2022-03-13 21:15:23 -0400

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -19,3 +19,33 @@ for (let i = 0; i < 7; i++) {
 //EJ solution:
 for (let line = "#"; line.length < 8; line += "#")
   console.log(line);
+
+/*
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
+
+//My solution:
+for (let num = 1; num <= 100; num++) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log('FizzBuzz'); 
+  } else if (num % 3 === 0) {
+    console.log('Fizz');
+  } else if (num % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(num);
+  }
+}
+
+//EJ Solution (doesn't include 'FizzBuzz' part):
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+
+/*
+
+*/
