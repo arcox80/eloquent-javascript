61d7a75c4947202e1f9eebc5bae1495c4ea8f17e
Andrew Cox <drew.r.cox@gmail.com>
2022-03-19 20:58:46 -0400

    Update chapter3.js

--- chapter3.js
+++ chapter3.js
@@ -1,6 +1,22 @@
-Write a function min that takes two arguments and returns their minimum.
+//Example of recursion
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return find(current + 5, `${history} + 5`) ||
+             find(current * 3, `${history} * 3`);
+    }
+  }
+  return find(1, "1");
+}
+console.log(findSolution(24));
 
-// Your code here.
+
+//Write a function min that takes two arguments and returns their minimum.
+
 
 console.log(min(0, 10));
 // → 0
