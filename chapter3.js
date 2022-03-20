3898a227d9456d64401d9adf2bc4771f8c487bb0
Andrew Cox <drew.r.cox@gmail.com>
2022-03-19 21:52:23 -0400

    Update chapter3.js

--- chapter3.js
+++ chapter3.js
@@ -33,11 +33,14 @@ console.log(min(0, -10));
 
 
 /*
-We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or 
+odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether 
+a positive whole number is even or odd:
 Zero is even.
 One is odd.
 For any other number N, its evenness is the same as N - 2.
-Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+Define a recursive function isEven corresponding to this description. The function should 
+accept a single parameter (a positive, whole number) and return a Boolean.
 */
 const isEven = (num) => {
   if (num < 0) {
@@ -63,3 +66,44 @@ function isEven(n) {
 console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
+
+/*
+Write a function countBs that takes a string as its only argument and returns a number 
+that indicates how many uppercase “B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second 
+argument that indicates the character that is to be counted (rather than counting only 
+uppercase “B” characters). Rewrite countBs to make use of this new function.
+*/
+
+const countChar = (str, char) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count ++;
+    }
+  }
+  return count;
+};
+
+const countBs = str => {
+  return countChar(str, 'B');
+};
+
+//EQs solution
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs('BBC'));
+console.log(countChar('kakkerlak', 'k'));
