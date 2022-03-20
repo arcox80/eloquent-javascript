f4352c2b97845e022e4f845b3a28bd6f90ba4fb0
Andrew Cox <drew.r.cox@gmail.com>
2022-03-19 21:41:53 -0400

    Update chapter3.js

--- chapter3.js
+++ chapter3.js
@@ -31,3 +31,35 @@ console.log(min(0, 10));
 console.log(min(0, -10));
 // → -10
 
+
+/*
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+Zero is even.
+One is odd.
+For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+*/
+const isEven = (num) => {
+  if (num < 0) {
+    num *= -1;
+  } 
+  if (num === 0) {
+    return true;
+  } else if (num === 1) {
+    return false;
+  } else {
+    return isEven(num - 2);
+  }
+};
+
+//EQ's solution
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
