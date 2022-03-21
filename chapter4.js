21b9a5422bc2a57ed68683728c9f4fe301ad3308
Andrew Cox <drew.r.cox@gmail.com>
2022-03-21 19:19:25 -0400

    Update chapter4.js

--- chapter4.js
+++ chapter4.js
@@ -184,21 +184,18 @@ objects with the same properties, where the values of the properties are equal w
 call to deepEqual. But you have to take one silly exception into account: because of a historical accident, 
 typeof null also produces "object".
 */
+//EJ solution
 const deepEqual = (val1, val2) => {
   if (val1 === val2) {
     return true;
   }
-  
   if (val1 === null || typeof val1 !== 'object' || val2 === null || typeof val2 !== 'object') {
     return false;
   }
-  
   let val1Keys = Object.keys(val1), val2Keys = Object.keys(val2);
-  
   if (val1Keys.length !== val2Keys.length) {
     return false;
   }
-  
   for (let key of val1Keys) {
     if (!val2Keys.includes(key) || !deepEqual(val1[key], val2[key])) {
       return false;
