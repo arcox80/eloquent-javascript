9d1aa34e7514cb6bc5796b78337640a09ff53e0f
Andrew Cox <drew.r.cox@gmail.com>
2022-03-20 20:18:05 -0400

    Create chapter4.js

---
+++ chapter4.js
@@ -0,0 +1,11 @@
+/*
+let journal = [];
+function addEntry(events, squirrel) {
+  journal.push({events, squirrel});
+}
+
+Note that the object added to the journal looks a little odd. Instead of declaring properties like 
+events: events, it just gives a property name. This is shorthand that means the same thing—if a property 
+name in brace notation isn’t followed by a value, its value is taken from the binding with the same name.
+*/
+
