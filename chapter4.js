61ae5b9b541943b85cb799744692ee08fdaf0f43
Andrew Cox <drew.r.cox@gmail.com>
2022-03-20 23:04:21 -0400

    Update chapter4.js

--- chapter4.js
+++ chapter4.js
@@ -82,3 +82,50 @@ console.log(range(1, 10));
 console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
 console.log(range(4, 4, -3));
+
+/*
+For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes
+an array as argument and produces a new array that has the same elements in the inverse order. The second, 
+reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing 
+its elements. Neither may use the standard reverse method.
+*/
+const reverseArray = array => {
+  const reversed = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i]);
+  }
+  return reversed;
+};
+
+const reverseArrayInPlace = array => {
+  let count = 2;
+  for (let i = 0; i < array.length; i++) {
+    let removed = array.splice(array.length - count, 1);
+    array.push(...removed);
+    count ++;
+  }
+  return array;
+};
+
+//EJ solution - 1.92% faster
+const reverseArray = array => {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+};
+
+const reverseArrayInPlace = array => {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+};
+
+console.log(reverseArray(['A', 'B', 'C']));
+let arrayValue = [1, 2, 3, 4, 5];
+console.log(reverseArrayInPlace(arrayValue));
+console.log(arrayValue);
