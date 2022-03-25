55664696d166b0c2a160a4d08dd356ee0f426be5
Andrew Cox <drew.r.cox@gmail.com>
2022-03-24 22:22:24 -0400

    Update chapter5.js

--- chapter5.js
+++ chapter5.js
@@ -1,4 +1,13 @@
-//Higher Order Functions - Functions that operate on other functions, either by taking them as arguments or by returning them
+/*
+In terms of what the computer is actually doing, these two approaches are also quite 
+different. The first will build up new arrays when running filter and map, whereas the 
+second computes only some numbers, doing less work. You can usually afford the readable 
+approach, but if you’re processing huge arrays, and doing so many times, the less abstract 
+style might be worth the extra speed.
+*/
+
+//Higher Order Functions - Functions that operate on other functions, either by taking 
+//them as arguments or by returning them
 function repeat(n, action) {
   for (let i=0; i < n; i++) {
       action(i);
@@ -32,3 +41,14 @@ for (let script of SCRIPTS) {
 }
 console.log(biggest);
 // → {name: "Han", …}
+
+function average(array) {
+  return array.reduce((a, b) => a + b) / array.length;
+}
+
+console.log(Math.round(average(
+  SCRIPTS.filter(s => s.living).map(s => s.year))));
+// → 1165
+console.log(Math.round(average(
+  SCRIPTS.filter(s => !s.living).map(s => s.year))));
+// → 204
